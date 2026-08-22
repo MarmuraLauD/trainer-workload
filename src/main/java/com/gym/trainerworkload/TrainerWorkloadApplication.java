e82f79f2105f6f0148bb184c618da413971595c4
LauD <marmuravlad10@gmail.com>
2026-08-23 01:01:11 +0300

    initialize project structure with Gradle, add application configuration and basic setup

---
+++ src/main/java/com/gym/trainerworkload/TrainerWorkloadApplication.java
@@ -0,0 +1,13 @@
+package com.gym.trainerworkload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerWorkloadApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TrainerWorkloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gym/trainerworkload/TrainerWorkloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.trainerworkload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerWorkloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
