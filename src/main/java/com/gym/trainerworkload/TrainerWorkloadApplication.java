10f67b3aa43372c6d17be4a8dd55230092519d67
LauD <marmuravlad10@gmail.com>
2026-08-23 02:53:36 +0300

    implement trainer workload management service and controller, enable service discovery

--- src/main/java/com/gym/trainerworkload/TrainerWorkloadApplication.java
+++ src/main/java/com/gym/trainerworkload/TrainerWorkloadApplication.java
@@ -2,8 +2,10 @@ package com.gym.trainerworkload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TrainerWorkloadApplication {
 
     static void main(String[] args) {
---
+++ src/main/java/com/gym/trainerworkload/controller/TrainerWorkloadController.java
@@ -0,0 +1,31 @@
+package com.gym.trainerworkload.controller;
+
+import com.gym.trainerworkload.dto.request.WorkloadRequest;
+import com.gym.trainerworkload.dto.response.TrainerWorkloadResponse;
+import com.gym.trainerworkload.service.TrainerWorkloadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/workloads")
+@RequiredArgsConstructor
+public class TrainerWorkloadController {
+
+    private final TrainerWorkloadService trainerWorkloadService;
+
+    @PostMapping
+    public void updateWorkload(@RequestBody WorkloadRequest request) {
+        trainerWorkloadService.updateWorkload(request);
+    }
+
+    @GetMapping("/{username}")
+    public TrainerWorkloadResponse getWorkload(@PathVariable String username) {
+        return trainerWorkloadService.getTrainerWorkload(username);
+    }
+
+}
