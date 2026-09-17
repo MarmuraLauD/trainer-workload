c8bc9a12378d7631ba7b7bade5d9c973fa9c7018
LauD <marmuravlad10@gmail.com>
2026-09-18 02:16:26 +0300

    feat: add Cucumber integration tests for Trainer Workload API and update build configuration

--- src/main/java/com/gym/trainerworkload/TrainerWorkloadApplication.java
+++ src/main/java/com/gym/trainerworkload/TrainerWorkloadApplication.java
@@ -2,9 +2,10 @@ package com.gym.trainerworkload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class TrainerWorkloadApplication {
 
